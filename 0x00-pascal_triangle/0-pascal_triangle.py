5d741243488e2b14c0f472f84c0cddbe157dae84
oussama El mazali <132847557+BANKLORD@users.noreply.github.com>
2024-05-27 11:34:24 +0100

    done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """pascal_triangle"""
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
